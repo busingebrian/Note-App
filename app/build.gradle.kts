187f0589600e0474cc4113ec43b656b1b8c7cc9a
Businge Brian <busingebrian@gmail.com>
2024-04-27 04:48:41 +0300

    updated the gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,7 +44,7 @@ dependencies {
     // ViewModel
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion")
 
-    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.core:core-ktx:1.13.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
