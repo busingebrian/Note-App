fc2f042ac7fa35593013254703741e559c91acb4
Businge Brian <busingebrian@gmail.com>
2024-05-17 10:10:14 +0300

    updated graddle.app library versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,16 +37,16 @@ android {
 
 dependencies {
     val roomVersion = "2.6.1"
-    val lifecycleVersion = "2.7.0"
+    val lifecycleVersion = "2.8.0"
     //room
     implementation ("androidx.room:room-runtime:$roomVersion")
     annotationProcessor ("androidx.room:room-compiler:$roomVersion")
     // ViewModel
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion")
 
-    implementation("androidx.core:core-ktx:1.13.0")
+    implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
